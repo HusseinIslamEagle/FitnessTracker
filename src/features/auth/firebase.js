1c3fb7511c1c025cce3f770da840fba85a519940
Hussein Ahmed <aha.hussein3@gmail.com>
2026-02-24 10:00:50 +0200

    Add setRecipes action to recipe store

--- src/features/auth/firebase.js
+++ src/features/auth/firebase.js
@@ -1,18 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCGEJFpt_gSAR0zNkKuJk-ynIH3AIRJmFk",
-  authDomain: "belghamdi-fitnesstracker.firebaseapp.com",
-  projectId: "belghamdi-fitnesstracker",
-  storageBucket: "belghamdi-fitnesstracker.firebasestorage.app",
-  messagingSenderId: "415381837360",
-  appId: "1:415381837360:web:0f0ea71fc924a6d9a83ff5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-
